07100b7a2e736c1ae41995427ee9f56fb2337601
Weskley Reis <weskleyff88004216@gmail.com>
2025-09-09 21:18:03 -0300

    Organizando toda estrutura do blog.

---
+++ src/repositories/post/index.ts
@@ -0,0 +1,5 @@
+import { JsonPostRepository  } from "./json-post-repository"
+import { PostRepository } from "./post-repository"
+
+
+export const postRepository: PostRepository = new JsonPostRepository()
