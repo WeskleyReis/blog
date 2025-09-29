777f41c0056f9b13a768141fd9058eaf452196e2
Weskley Reis <weskleyff88004216@gmail.com>
2025-09-29 15:16:13 -0300

    Correção para Deploy

--- src/utils/get-zoid-error-messages.ts
+++ src/utils/get-zoid-error-messages.ts
@@ -1,10 +1,21 @@
-import { ZodFormattedError } from 'zod';
+import { ZodFormattedError } from "zod";
 
-export function getZodErrorMessages<T>(error: ZodFormattedError<T>): string[] {
+export function getZodErrorMessages<T>(
+  error: ZodFormattedError<T>
+): string[] {
   return Object.values(error)
-    .map(field => {
+    .map((field) => {
       if (Array.isArray(field)) return field;
-      return field?._errors || [];
+
+      if (
+        typeof field === "object" &&
+        field !== null &&
+        "_errors" in field
+      ) {
+        return (field as { _errors: string[] })._errors;
+      }
+
+      return [];
     })
     .flat()
     .filter(Boolean);
